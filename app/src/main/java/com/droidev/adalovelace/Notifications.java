47561d62a9f82b64860f7ba9a16c95139ddff8b9
none <none@>
2023-12-23 17:25:39 -0300

    fixed bug that made the app crash when enabling notifications, the layouts now should be good to use even on small screen devices.

--- app/src/main/java/com/droidev/adalovelace/Notifications.java
+++ app/src/main/java/com/droidev/adalovelace/Notifications.java
@@ -31,7 +31,7 @@ public class Notifications {
 
         Intent intent = new Intent(context, CardanoActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        @SuppressLint("UnspecifiedImmutableFlag") PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ada_notification_icon)
